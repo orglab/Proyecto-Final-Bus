8c8c01af14e41d14afa0ec01cea85e94aa15a09d
Manyork Gomez <manyor-93@hotmail.es>
2019-04-05 00:27:40 -0600

    Commit Base proyecto otro

--- src/NewMain.java
+++ src/NewMain.java
@@ -19,6 +19,8 @@ public class NewMain {
         // TODO code application logic here
        DataBase base = new DataBase();
        base.conectar();
+       ///PincheComentario
+       
        
        
     }
